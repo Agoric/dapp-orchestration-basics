3ea174d77a2f73adebdb416ced8a36c2c69de4bc
Dan Connolly <dckc@madmode.com>
2024-06-25 09:52:52 -0500

    test(orca-contract): test orca.proposal core-eval

--- contract/test/test-orca-contract.js
+++ contract/test/test-orca-contract.js
@@ -8,6 +8,12 @@ import { E, Far } from '@endo/far';
 // import { makeCopyBag } from '@endo/patterns';
 import { makeNodeBundleCache } from '@endo/bundle-source/cache.js';
 import { makeZoeKitForTest } from '@agoric/zoe/tools/setup-zoe.js';
+import { startOrcaContract } from '../src/orca.proposal.js';
+
+import { makeMockTools } from './boot-tools.js';
+import { getBundleId } from '../tools/bundle-tools.js';
+import { startOrchCoreEval } from '../tools/startOrch.js';
+
 // import { AmountMath, AssetKind, makeIssuerKit } from '@agoric/ertp';
 // import { prepareOrchestrationTools } from '@agoric/orchestration'
 
@@ -24,6 +30,9 @@ import { makeZoeKitForTest } from '@agoric/zoe/tools/setup-zoe.js';
 
 const myRequire = createRequire(import.meta.url);
 const contractPath = myRequire.resolve(`../src/orca.contract.js`);
+const scriptRoot = {
+  orca: myRequire.resolve('../src/orca.proposal.js'),
+};
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<makeTestContext>>>} */
 // @ts-expect-error - XXX what's going on here??
@@ -35,13 +44,15 @@ const test = anyTest;
  * See test-bundle-source.js for basic use of bundleSource().
  * Here we use a bundle cache to optimize running tests multiple times.
  *
- * @param {unknown} _t
+ * @param {import('ava').ExecutionContext} t
  */
-const makeTestContext = async _t => {
+const makeTestContext = async t => {
   const { zoeService: zoe, feeMintAccess } = makeZoeKitForTest();
   const bundleCache = await makeNodeBundleCache('bundles/', {}, s => import(s));
   const bundle = await bundleCache.load(contractPath, 'orca');
-  return { zoe, bundle, bundleCache, feeMintAccess };
+
+  const tools = await makeMockTools(t, bundleCache);
+  return { zoe, bundle, bundleCache, feeMintAccess, ...tools };
 };
 
 test.before(async t => (t.context = await makeTestContext(t)));
@@ -72,4 +83,35 @@ test('Start Orca contract', async t => {
   );
   t.log('started:', instance);
   t.truthy(instance);
+});
+
+test('Start Orca contract using core-eval', async t => {
+  const { runCoreEval, installBundles, makeQueryTool } = t.context;
+
+  t.log('run core-eval to start (dummy) orchestration');
+  await runCoreEval({
+    name: 'start-orchestration',
+    behavior: startOrchCoreEval,
+    entryFile: 'not used',
+    config: {},
+  });
+
+  const bundles = await installBundles({ orca: contractPath });
+
+  t.log('run orca core-eval');
+  const bundleID = getBundleId(bundles.orca);
+  const name = 'orca';
+  const { status } = await runCoreEval({
+    name,
+    behavior: startOrcaContract,
+    entryFile: scriptRoot.orca,
+    config: { options: { orca: { bundleID } } },
+  });
+  t.is(status, 'PROPOSAL_STATUS_PASSED');
+
+  const qt = makeQueryTool();
+  const instance = await qt
+    .queryData('published.agoricNames.instance')
+    .then(es => Object.fromEntries(es));
+  t.log(instance[name]);
 });
