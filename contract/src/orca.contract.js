4469a1669645f7d1d3f16fe427949f9092bdbc11
Dan Connolly <dckc@madmode.com>
2024-06-25 09:46:01 -0500

    style(orca.contract): lint

--- contract/src/orca.contract.js
+++ contract/src/orca.contract.js
@@ -2,21 +2,19 @@
 import { M } from '@endo/patterns';
 import '@agoric/zoe/exported.js';
 import {
-    defineERecorderKit,
-    prepareRecorderKitMakers,
-    provideAll
+  defineERecorderKit,
+  prepareRecorderKitMakers,
+  provideAll,
 } from '@agoric/zoe/src/contractSupport/index.js';
 
 import { makeTracer, StorageNodeShape } from '@agoric/internal';
 import { makeDurableZone } from '@agoric/zone/durable.js';
 // import { heapVowE as E, prepareVowTools } from '@agoric/vow/vat.js';
 
-
 // import { prepareStakingAccountKit } from '@agoric/orchestration/src/exos/stakingAccountKit.js';
 // import { prepareCosmosOrchestrationAccountKit, prepareCosmosOrchestrationAccount } from '@agoric/orchestration/src/exos/cosmosOrchestrationAccount.js';
 // import { prepareCosmosOrchestrationAccount } from '@agoric/orchestration/src/exos/cosmos-orchestration-account.js';
 
-  
 const trace = makeTracer('OrchDev1');
 
 /**
@@ -27,73 +25,64 @@ const trace = makeTracer('OrchDev1');
  */
 
 export const meta = harden({
-    privateArgsShape: {
-      orchestration: M.remotable('orchestration'),
-      storageNode: StorageNodeShape,
-      marshaller: M.remotable('Marshaller'),
-      timer: M.remotable('TimerService'),
-    },
+  privateArgsShape: {
+    orchestration: M.remotable('orchestration'),
+    storageNode: StorageNodeShape,
+    marshaller: M.remotable('Marshaller'),
+    timer: M.remotable('TimerService'),
+  },
 });
 
 export const privateArgsShape = meta.privateArgsShape;
 
-
-export const terms = harden({
-    
-});
-
+export const terms = harden({});
 
 /**
  *
  * @param {ZCF<StakeAtomTerms>} zcf
  * @param {{
-*  orchestration: OrchestrationService;
-*  storageNode: StorageNode;
-*  marshaller: Marshaller;
-*  timer: TimerService;
-* }} privateArgs
-* @param {Baggage} baggage
-*/
-export const start = async (zcf , privateArgs, baggage) => {
+ *  orchestration: OrchestrationService;
+ *  storageNode: StorageNode;
+ *  marshaller: Marshaller;
+ *  timer: TimerService;
+ * }} privateArgs
+ * @param {Baggage} baggage
+ */
+export const start = async (zcf, privateArgs, baggage) => {
+  console.log('CONTRACT START FUNCTION...');
+  const { orchestration, marshaller, storageNode, timer } = privateArgs;
+  console.log('CONTRACT START FUNCTION 2...');
+  const zone = makeDurableZone(baggage);
+  console.log('CONTRACT START FUNCTION 3...');
+  const { makeRecorderKit } = prepareRecorderKitMakers(baggage, marshaller);
+  console.log('CONTRACT START FUNCTION 4...abc');
+  // TODO: fix Possible HTML comment rejected?
+  // const makeStakingAccountKit = prepareCosmosOrchestrationAccountKit(
+  //     baggage,
+  //     makeRecorderKit,
+  //     zcf,
+  // );
 
-    console.log("CONTRACT START FUNCTION...")
-    const { orchestration, marshaller, storageNode, timer } = privateArgs;
-    console.log("CONTRACT START FUNCTION 2...")
-    const zone = makeDurableZone(baggage);
-    console.log("CONTRACT START FUNCTION 3...")
-    const { makeRecorderKit } = prepareRecorderKitMakers(baggage, marshaller);
-    console.log("CONTRACT START FUNCTION 4...abc")
-    // TODO: fix Possible HTML comment rejected?
-    // const makeStakingAccountKit = prepareCosmosOrchestrationAccountKit(
-    //     baggage,
-    //     makeRecorderKit,
-    //     zcf,
-    // );
+  // const vowTools = prepareVowTools(zone.subZone('vows'));
+  // const makeCosmosOrchestrationAccount = prepareCosmosOrchestrationAccount(
+  //     zone,
+  //     makeRecorderKit,
+  //     vowTools,
+  //     zcf,
+  //   );
 
-    // const vowTools = prepareVowTools(zone.subZone('vows'));
-    // const makeCosmosOrchestrationAccount = prepareCosmosOrchestrationAccount(
-    //     zone,
-    //     makeRecorderKit,
-    //     vowTools,
-    //     zcf,
-    //   );
+  console.log('CONTRACT START FUNCTION 5...');
+  const publicFacet = zone.exo(
+    'Orca Public Facet',
+    M.interface('StakeAtomI', {
+      // makeAccount: M.callWhen().returns(M.remotable('ChainAccount')),
+      // makeAcountInvitationMaker: M.call().returns(M.promise()),
+    }),
+    {},
+  );
 
-
-
-    console.log("CONTRACT START FUNCTION 5...")
-    const publicFacet = zone.exo(
-        'Orca Public Facet', 
-        M.interface('StakeAtomI', {
-            // makeAccount: M.callWhen().returns(M.remotable('ChainAccount')),
-            // makeAcountInvitationMaker: M.call().returns(M.promise()),
-        }),
-        {
-            
-        },
-    );
-
-    return harden({publicFacet});
-}
+  return harden({ publicFacet });
+};
 
 // harden(start)
 /** @typedef {typeof start} OrcaSF */
