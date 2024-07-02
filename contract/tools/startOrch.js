567352eff64a835630645717dd9495feb1573f70
Dan Connolly <dckc@madmode.com>
2024-07-03 00:36:17 +0200

    chore(startOrch): provide (dummy) localchain too

--- contract/tools/startOrch.js
+++ contract/tools/startOrch.js
@@ -1,8 +1,16 @@
 // @ts-check
 
 import { Far } from '@endo/far';
+import { makeTracer } from '../src/tools/debug.js';
+
+const trace = makeTracer('startOrch');
 
 /** @param {BootstrapPowers} powers */
-export const startOrchCoreEval = async ({ produce: { orchestration } }) => {
+export const startOrchCoreEval = async ({
+  produce: { orchestration, localchain },
+}) => {
+  trace('startOrchCoreEval');
   orchestration.resolve(Far('DummyOrchestration'));
+  localchain.resolve(Far('DummyLocalchain'));
+  trace('startOrchCoreEval done');
 };
