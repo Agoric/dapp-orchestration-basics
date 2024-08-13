72c6416f9c4da069a9dfc9969b12949c6f3b4227
jovonni <jovonnipharr@gmail.com>
2024-08-13 15:29:29 -0400

    daisy selector

--- ui/src/components/Orchestration/ChainSelector.tsx
+++ ui/src/components/Orchestration/ChainSelector.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ChainSelector = ({ setSelectedChain }) => (
   <select
-    className="select select-bordered mb-4 rounded-lg border-0 w-full max-w-xs text-lg"
-    onChange={(e) => setSelectedChain(e.target.value)}
+  className="select select-bordered daisyui-input-bordered daisyui-focus:ring-blue-500 daisyui-focus:border-blue-500 bg-black-500 text-white text-lg py-2 px-4 rounded-lg w-full"
+  onChange={(e) => setSelectedChain(e.target.value)}
   >
-    <option disabled selected>Select Remote Chain</option>
+    <option disabled selected>Select Chain</option>
     <option value="osmosis">Osmosis</option>
     <option value="agoric">Agoric</option>
   </select>
