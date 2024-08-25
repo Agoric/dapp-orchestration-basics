e6bb7650cf405818c10be1fdf86da806d4e84ef1
jovonni <jovonnipharr@gmail.com>
2024-08-24 21:04:16 -0400

    chore: added testing support from multichain-testing

---
+++ contract/tools/registry.js
@@ -0,0 +1,23 @@
+import { useRegistry, useChain, ConfigContext } from 'starshipjs';
+import path from 'path';
+
+export const makeGetFile = () => (filePath) =>
+  {
+    return path.join(path.dirname(new URL(import.meta.url).pathname), filePath);
+  }
+
+export const makeSetupRegistry = (getFile) => {
+  let initialized = false;
+
+  const setupRegistry = async ({ config = '../config.yaml' } = {}) => {
+    if (initialized) return { useChain };
+    const configFile = getFile(config);
+    console.log("configFile", configFile)
+    const fetcher = await useRegistry(configFile);
+    await ConfigContext.init(configFile, fetcher);
+    initialized = true;
+    return { useChain };
+  };
+
+  return setupRegistry;
+};
