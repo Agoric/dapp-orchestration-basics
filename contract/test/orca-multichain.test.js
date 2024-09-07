43e200313cd0149960872af3f695fe2a1c075af2
jovonni <jovonnipharr@gmail.com>
2024-09-07 08:48:18 -0400

    chore(orca-multichain.test.js): added makeCreateAndFundScenario to test makeAccountAndFund with denom to invoke remoteChain.getBalance. Success with test using osmosis, and getting brands for tests from vstorage directly

--- contract/test/orca-multichain.test.js
+++ contract/test/orca-multichain.test.js
@@ -4,6 +4,7 @@ import { E, getInterfaceOf } from '@endo/far';
 import path from 'path';
 import { commonSetup, chainConfig } from './support.js';
 import { makeDoOffer } from '../tools/e2e-tools.js';
+import { AmountMath, makeIssuerKit } from '@agoric/ertp';
 
 const test = anyTest;
 
@@ -48,6 +49,7 @@ const makeAccountScenario = test.macro({
     } = t.context;
 
     const agoricAddr = wallets[chainName];
+    console.log("agoricAddr:", agoricAddr)
     const wdUser1 = await provisionSmartWallet(agoricAddr, {
       BLD: 100n,
       IST: 100n,
@@ -111,6 +113,109 @@ const makeAccountScenario = test.macro({
   },
 });
 
-test.serial(makeAccountScenario, 'agoric');
-test.serial(makeAccountScenario, 'cosmoshub');
-test.serial(makeAccountScenario, 'osmosis');
+const makeCreateAndFundScenario = test.macro({
+  title: (_, chainName, denom) => `Create and fund account on ${chainName} with denom: ${denom}`,
+  exec: async (t, chainName, denom) => {
+    const config = chainConfig[chainName];
+    if (!config) return t.fail(`Unknown chain: ${chainName}`);
+
+    console.log(`testing makeCreateAndFundScenario for chain ${chainName}, and denom ${denom}`);
+    const {
+      wallets,
+      provisionSmartWallet,
+      vstorageClient,
+      retryUntilCondition,
+    } = t.context;
+
+    const agoricAddr = wallets[chainName];
+    console.log("agoricAddr:", agoricAddr)
+    const wdUser1 = await provisionSmartWallet(agoricAddr, {
+      BLD: 100n,
+      IST: 100n,
+    });
+
+    t.log(`Provisioning Agoric smart wallet for ${agoricAddr}`);
+
+    const doOffer = makeDoOffer(wdUser1);
+    t.log(`${chainName} makeCreateAndFund offer`);
+    const offerId = `${chainName}-makeCreateAndFund-${Date.now()}`;
+
+    t.log('Before doOffer');
+
+    // const { mint, issuer, brand } = makeIssuerKit('BLD');
+
+    // const issuers = {
+    //   BLDIssuer: issuer,
+    // };
+
+    const brands = await vstorageClient.queryData(
+      'published.agoricNames.brand',
+    );
+    const brand = Object.fromEntries(brands).BLD;
+
+    console.log("brand::", brand)
+
+    const amount = AmountMath.make(brand, 10n);
+
+    await doOffer({
+      id: offerId,
+      invitationSpec: {
+        source: 'agoricContract',
+        instancePath: [contractName],
+        callPipe: [['makeCreateAndFundInvitation']], 
+      },
+      offerArgs: { chainName, denom },
+      proposal: {
+        give: { Deposit: amount },
+        want: {},
+        exit: { onDemand: null },
+      },
+    });
+    
+    const currentWalletRecord = await retryUntilCondition(
+      () => vstorageClient.queryData(`published.wallet.${agoricAddr}.current`),
+      ({ offerToPublicSubscriberPaths }) =>
+        Object.fromEntries(offerToPublicSubscriberPaths)[offerId],
+      `${offerId} continuing invitation is in vstorage`,
+    );
+
+    console.log('currentWalletRecord', currentWalletRecord);
+
+    const offerToPublicSubscriberMap = Object.fromEntries(
+      currentWalletRecord.offerToPublicSubscriberPaths,
+    );
+
+    const address = offerToPublicSubscriberMap[offerId]?.account
+      .split('.')
+      .pop();
+    t.log('got address:', address);
+    t.regex(
+      address,
+      new RegExp(`^${config.expectedAddressPrefix}1`),
+      `address for ${chainName} is valid`,
+    );
+
+    const latestWalletUpdate = await vstorageClient.queryData(
+      `published.wallet.${agoricAddr}`,
+    );
+    t.log('latest wallet update', latestWalletUpdate);
+    t.like(
+      latestWalletUpdate.status,
+      {
+        id: offerId,
+        numWantsSatisfied: 1,
+        result: 'UNPUBLISHED',
+        error: undefined,
+      },
+      'wallet offer satisfied without errors',
+    );
+  },
+});
+
+// test.serial(makeAccountScenario, 'agoric');
+// test.serial(makeAccountScenario, 'cosmoshub');
+// test.serial(makeAccountScenario, 'osmosis');
+
+// test.serial(makeCreateAndFundScenario, 'agoric', 'ubld');
+// use IBC/E7827844CB818EE9C4DB2C159F1543FF62B26213B44CE8029D5CEFE52F0EE596
+test.serial(makeCreateAndFundScenario, 'osmosis', 'ubld');
