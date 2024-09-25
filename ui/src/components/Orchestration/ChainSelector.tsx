e04444ad48c2cb50cc24b5e3c73f7df99d020d2e
Mudassir Shabbir <mudassir@agoric.com>
2024-09-25 09:08:58 +0500

    fix: change text color in chain selector

--- ui/src/components/Orchestration/ChainSelector.tsx
+++ ui/src/components/Orchestration/ChainSelector.tsx
@@ -1,6 +1,6 @@
 const ChainSelector = ({ setSelectedChain }) => (
   <select
-    className="select select-bordered daisyui-focus:ring-blue-500 daisyui-focus:border-blue-500 bg-black-500 daisyui-input-bordered w-full rounded-lg px-4 py-2 text-lg text-white"
+    className="select select-bordered daisyui-focus:ring-blue-500 daisyui-focus:border-blue-500 bg-black-500 daisyui-input-bordered text-blue w-full rounded-lg px-4 py-2"
     onChange={e => setSelectedChain(e.target.value)}
   >
     <option disabled selected>
