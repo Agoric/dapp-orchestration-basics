cface9d8bd9400f1f4a51f58b54a821a098b22f5
jovonni <jovonnipharr@gmail.com>
2024-09-25 02:35:21 -0400

    fix(deploy.js): Added makeDeployBuilderE2E to allow both e2e script and orca-multichain.test.js to run

--- contract/tools/deploy.js
+++ contract/tools/deploy.js
@@ -25,7 +25,41 @@ export const makeDeployBuilder = (tools, readJSON, execa) =>
 
     console.log('installing bundles');
     await tools.installBundles(
+      // plan.bundles.map(b => `/tmp/contracts/${b.bundleID}.json`),
+      plan.bundles.map(b => `/root/${b.bundleID}.json`),
+      console.log,
+    );
+
+    console.log('executing proposal');
+    await tools.runCoreEval({
+      name: plan.name,
+      description: `${plan.name} proposal`,
+    });
+  };
+
+export const makeDeployBuilderE2E = (tools, readJSON, execa) =>
+  async function deployBuilder(builder) {
+    console.log(`building plan: ${builder}`);
+    // build the plan
+    const { stdout } = await execa`agoric run ${builder}`;
+    const match = stdout.match(/ (?<name>[-\w]+)-permit.json/);
+    if (!(match && match.groups)) {
+      throw new Error('no permit found');
+    }
+    const plan = await readJSON(`./${match.groups.name}-plan.json`);
+    console.log(plan);
+
+    console.log('copying files to container');
+    tools.copyFiles([
+      nodeRequire.resolve(`../${plan.script}`),
+      nodeRequire.resolve(`../${plan.permit}`),
+      ...plan.bundles.map(b => b.fileName),
+    ]);
+
+    console.log('installing bundles');
+    await tools.installBundles(
       plan.bundles.map(b => `/tmp/contracts/${b.bundleID}.json`),
+      // plan.bundles.map(b => `/root/${b.bundleID}.json`),
       console.log,
     );
 
